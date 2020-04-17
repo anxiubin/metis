04942969d3f38e2521b032150c627ffdbee3a3fd
anxiubin <tnqls0120@naver.com>
2020-04-17 16:59:07 +0900

    ENH: add all files and images

---
+++ index.js
@@ -0,0 +1,98 @@
+//TOKEN CHART 
+
+
+const options = {
+    chart: {
+      type: 'donut',
+      width: '600px',
+      height: '600px'
+    },
+    series: [75,20,5],
+    labels: ['company', 'reward', 'SI'],
+    fill: {
+        colors: ['#616de7', '#FF7A31', '#58BBFF'],
+    },
+    legend: {
+        show: false
+    },
+    tooltip: {
+        enabled: true,
+        fillSeriesColor: true
+    },
+    dataLabels: {
+        enabled: true,
+        textAnchor: 'middle',
+        formatter: function (val) {
+            let name = '';
+            switch (val) {
+                case 75:
+                    name = 'Company owned';
+                    break;
+                case 20:
+                    name = 'Learning reward';
+                    break;
+                case 5:
+                    name = 'SI';
+                    break;                
+            }
+          return `${name} ${val}%`
+        },
+        style: {
+            fontSize: '20px',
+            colors: ['#fff', '#fff', '#fff']
+        }
+    },
+    plotOptions: {
+        pie: {
+          donut: {
+            size: '40%',
+          }
+        }
+    }
+    
+}
+
+  
+const chart = new ApexCharts(document.querySelector("#chart"), options);
+  
+chart.render();
+
+//scroll event
+
+const chartBoxEl = document.querySelector('.chartBox');
+
+function scrollView() { 
+    const chartIntAt = (window.scrollY + window.innerHeight) - chartBoxEl.offsetHeight / 4;
+    const chartBottom = chartBoxEl.offsetTop + chartBoxEl.offsetHeight; 
+    const isHalfShown = chartIntAt > chartBoxEl.offsetTop; 
+    const isNotScrolledPast = window.scrollY < chartBottom; 
+    
+    if (isHalfShown && isNotScrolledPast) { 
+        chartBoxEl.classList.add('show'); 
+        } else { 
+            chartBoxEl.classList.remove('show');
+        }
+
+    console.log('scrolled', isHalfShown, isNotScrolledPast);
+}
+
+
+//thorottle
+
+function throttle(fn, delay) {
+    let timer
+    return function() {
+        if (!timer){
+            timer = setTimeout(() => {
+                timer = null
+                fn.apply(this, arguments)
+            }, delay)
+        }
+    }
+}
+
+window.addEventListener('scroll', throttle(scrollView, 500));
+
+
+
+
